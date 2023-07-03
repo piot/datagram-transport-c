128ebec3a6c21eebf4ec7c8b743ebddd7b5e831d
Peter Bjorklund <piot@hotmail.com>
2023-07-03 13:09:23 +0200

    fix: add types.h with max size constant

---
+++ src/include/datagram-transport/types.h
@@ -0,0 +1,13 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Peter Bjorklund. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+#ifndef DATAGRAM_TRANSPORT_TYPES_H
+#define DATAGRAM_TRANSPORT_TYPES_H
+
+#include <stddef.h>
+
+static const size_t datagramTransportMaxSize = 1200;
+#define DATAGRAM_TRANSPORT_MAX_SIZE ((size_t) 1200)
+
+#endif
